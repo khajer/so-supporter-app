77b7b572659b0a02ce9637e7a705e33a2cc693b5
itsara <khajer@gmail.com>
2024-10-08 22:44:54 +0700

    add simple account data

--- programs/so-supporter-app/src/lib.rs
+++ programs/so-supporter-app/src/lib.rs
@@ -6,11 +6,37 @@ declare_id!("6YnirNoEnw8Tp7ZbaH1DV8KBwN7ScbHZNXQVeqaAZYct");
 pub mod so_supporter_app {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    // pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    //     msg!("Greetings from: {:?}", ctx.program_id);
+    //     Ok(())
+    // }
+    pub fn create_post(
+        ctx: Context<CreatePost>,
+        title: String,
+        img_url: String,
+        desc: String,
+    ) -> Result<()> {
+        let user_account = &mut ctx.accounts.user_account;
+        user_account.title = title;
+        user_account.img_url = img_url;
+        user_account.desc = desc;
+
         Ok(())
     }
 }
 
+#[account]
+pub struct PostAccount {
+    title: String,
+    img_url: String,
+    desc: String,
+}
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct CreatePost<'info> {
+    #[account(init, payer = user, space = 8 + std::mem::size_of::<PostAccount>() )]
+    user_account: Account<'info, PostAccount>,
+    #[account(mut)]
+    user: Signer<'info>,
+    system_program: Program<'info, System>,
+}
--- tests/so-supporter-app.ts
+++ tests/so-supporter-app.ts
@@ -1,16 +1,32 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { SoSupporterApp } from "../target/types/so_supporter_app";
+import { expect } from "chai";
 
 describe("so-supporter-app", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
+  const myAccount = anchor.web3.Keypair.generate();
   const program = anchor.workspace.SoSupporterApp as Program<SoSupporterApp>;
 
+  // it("Is initialized!", async () => {
+  //   // Add your test here.
+  //   const tx = await program.methods.initialize().rpc();
+  //   console.log("Your transaction signature", tx);
+  // });
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const tx = await program.methods.createPost("title", "image/url", "desc").accounts({
+      userAccount: myAccount.publicKey
+    }).signers([myAccount]).rpc();
     console.log("Your transaction signature", tx);
+
+    const myAcc = await program.account.postAccount.fetch(myAccount.publicKey);
+    // console.log(myAcc);
+    expect("title").equal(myAcc.title);
+
+
   });
+
 });
